b83b054eb948b8652600ae1e3483944dc8730e61
Garrett Biernat <garrett@gdbmedia.net>
2016-05-13 17:17:52 -0700

    added heroku stuff

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,6 +5,18 @@ import static spark.Spark.*;
 
 public class App {
   public static void main(String[] args) {
+
+    ProcessBuilder process = new ProcessBuilder();
+    Integer port;
+    if (process.environment().get("PORT") != null) {
+        port = Integer.parseInt(process.environment().get("PORT"));
+    } else {
+        port = 4567;
+    }
+
+   setPort(port);
+
+
     staticFileLocation("/public");
     String layout = "templates/layout.vtl";
 
@@ -127,6 +139,6 @@ public class App {
       response.redirect("/bands/" + request.params("id"));
       return null;
     });
-//UPDATE 
+//UPDATE
   }
 }
--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -1,5 +1,5 @@
 import org.sql2o.*;
 
 public class DB {
-  public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/band_tracker", null, null);
+  public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-54-227-240-164.compute-1.amazonaws.com:5432/d9ft6eatkdvhfn?sslmode=require&user=czxyijixlzmsra&password=CEid2epfW9FfdJXEmxQm1z6QFp", null, null);
 }
